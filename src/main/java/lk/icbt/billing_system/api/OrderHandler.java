7102b83a1b54f5b4899807a79eaac6d6a9e5e750
DhanushkaTa <tharindu@leadinnovationz.com>
2025-08-18 23:53:35 +0530

    app order handler in mappings

--- src/main/java/lk/icbt/billing_system/api/OrderHandler.java
+++ src/main/java/lk/icbt/billing_system/api/OrderHandler.java
@@ -1,6 +1,29 @@
 package lk.icbt.billing_system.api;
 
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import lk.icbt.billing_system.dto.ItemDTO;
+import lk.icbt.billing_system.dto.OrderDTO;
+import lk.icbt.billing_system.dto.RespondsDTO;
+
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class OrderHandler extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        Jsonb jsonb = JsonbBuilder.create();
+        OrderDTO orderDTO = jsonb.fromJson(req.getReader(), OrderDTO.class);
+
+        System.out.println(orderDTO.toString());
+        System.out.println(orderDTO.getOrderDetails().toString());
+
+        jsonb.toJson(new RespondsDTO(400, "Item not updated!",""), resp.getWriter());
+
+    }
 }
