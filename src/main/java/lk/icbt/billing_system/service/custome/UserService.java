2f933624b28824ef1592a271bed1a18034a28069
DhanushkaTa <tharindu@leadinnovationz.com>
2025-08-19 15:29:40 +0530

    add userService and userServiceImpl class

---
+++ src/main/java/lk/icbt/billing_system/service/custome/UserService.java
@@ -0,0 +1,6 @@
+package lk.icbt.billing_system.service.custome;
+
+import lk.icbt.billing_system.service.SuperService;
+
+public interface UserService extends SuperService {
+}
---
+++ src/main/java/lk/icbt/billing_system/service/custome/UserServiceImpl.java
@@ -0,0 +1,4 @@
+package lk.icbt.billing_system.service.custome;
+
+public class UserServiceImpl implements UserService{
+}
