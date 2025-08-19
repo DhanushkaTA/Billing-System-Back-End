8c4e5918903979eedcae7ce8c4eb045dd180b655
DhanushkaTa <tharindu@leadinnovationz.com>
2025-08-19 15:34:03 +0530

    inject userAdo

--- src/main/java/lk/icbt/billing_system/service/custome/impl/UserServiceImpl.java
+++ src/main/java/lk/icbt/billing_system/service/custome/impl/UserServiceImpl.java
@@ -1,5 +1,8 @@
 package lk.icbt.billing_system.service.custome.impl;
 
+import lk.icbt.billing_system.dao.DaoFactory;
+import lk.icbt.billing_system.dao.DaoTypes;
+import lk.icbt.billing_system.dao.custome.UserDAO;
 import lk.icbt.billing_system.dto.UserDTO;
 import lk.icbt.billing_system.service.custome.UserService;
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -9,6 +12,8 @@ import java.sql.Connection;
 public class UserServiceImpl implements UserService {
 
     private final BasicDataSource bds;
+
+    private final UserDAO userDAO = (UserDAO) DaoFactory.getDaoFactory().getDAO(DaoTypes.USER);
 
     public UserServiceImpl(BasicDataSource bds){
         this.bds=bds;
